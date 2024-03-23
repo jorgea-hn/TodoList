f6c8830a3051024697b101d53cc54f13af714d63
jorgea-hn <jorgeahenriqueznovoa@gmail.com>
2024-03-23 17:09:37 -0500

    Boton de eliminar y refresco de pagina

--- frontend/src/app/components/TaskCard.jsx
+++ frontend/src/app/components/TaskCard.jsx
@@ -1,4 +1,21 @@
+"use client"
+import { useRouter } from "next/navigation";
+
 function TaskCard({task}) {
+    const router = useRouter();
+
+    const handleDelete = async (id) => {
+        
+        if (window.confirm("Quieres eliminar esta tarea?")){
+            const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/tasks/${id}`,{
+                method: "DELETE"
+            })
+            if (res.status===204){
+                router.refresh();
+            }
+        }
+    }
+
     return (
         <div className="bg-slate-500 px-4 py-3 mb-2 rounded-md flex justify-between items-center">
             <div>
@@ -6,7 +23,7 @@ function TaskCard({task}) {
                 <p>{task.description}</p>
             </div>
             <div className=" flex justify-between gap-x-2">
-                <button className="bg-red-500 text-white rounded-md p-2">Eliminar</button>
+                <button className="bg-red-500 text-white rounded-md p-2" onClick={()=> handleDelete(task.id)}>Eliminar</button>
                 <button className="bg-indigo-500 text-white rounded-md p-2">Actualizar</button>
             </div>
         </div>
